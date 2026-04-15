c6547282d21a1c606e515b162c3fc3a1908f6e24
hejinhui <1500853783@qq.com>
2026-04-15 16:26:08 +0800

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
@@ -16,6 +16,7 @@ const feedbackRoutes = require('./routes/feedback');
 const communityRoutes = require('./routes/community');
 app.use('/', authRoutes);
 app.use('/', feedbackRoutes);
+app.use('/', communityRoutes);
 app.use('/community', communityRoutes);
 app.use('/commity', communityRoutes);
 
